350b50ae0955608c3d3f3761f9916916bec833d6
DeadOnGames <deadongamesonline@gmail.com>
2025-06-20 09:10:24 +0100

    Add context menu changes

--- Assets/MissionSystem/MissionStatePattern.cs
+++ Assets/MissionSystem/MissionStatePattern.cs
@@ -6,9 +6,13 @@ public class MissionStatePattern : MonoBehaviour, IMissionContext
 {
     IMissionState currentState = new MissionLockedState();
 
+    [ContextMenu("StartMission()")]
     public virtual void StartMission() => currentState.StartMission(this);
+    [ContextMenu("RestartMission()")]
     public virtual void RestartMission() => currentState.RestartMission(this);
+    [ContextMenu("CompleteMission()")]
     public virtual void CompleteMission() => currentState.CompleteMission(this);
+    [ContextMenu("FailMission()")]
     public virtual void FailMission() => currentState.FailMission(this);
 
     void IMissionContext.SetState(IMissionState newState)
